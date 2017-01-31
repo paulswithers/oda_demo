59fb7555aa0099ea37f2b9ed5cfec1681e2a767d
Paul Withers <paulswithers@hotmail.co.uk>
2017-01-31 15:21:43 +0000

    Adding call to super

--- OdaDemoAppLiberty/src/main/java/org/openntf/domino/demoApp/xworlds/Filter.java
+++ OdaDemoAppLiberty/src/main/java/org/openntf/domino/demoApp/xworlds/Filter.java
@@ -1,5 +1,11 @@
 package org.openntf.domino.demoApp.xworlds;
 
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
 import org.openntf.xworlds.appservers.webapp.XWorldsRequestsFilter;
@@ -9,6 +15,12 @@ public class Filter extends XWorldsRequestsFilter {
 
 	public Filter() {
 
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		super.doFilter(request, response, chain);
 	}
 
 }
